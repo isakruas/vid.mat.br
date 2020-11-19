5ec560841e48e61c6d45a3cc4c6b96dbbee404ac
isakruas <38894795+isakruas@users.noreply.github.com>
2020-11-19 09:19:58 -0300

    Update index.php

--- exercicios_resolvidos/analise_real/limites_de_funcoes/index.php
+++ exercicios_resolvidos/analise_real/limites_de_funcoes/index.php
@@ -75,12 +75,7 @@
 			                            <!--<p style="display: none; background: #eee; padding: 0.5cm " id="v_1_">
 			                                Em construção: videoaula
 			                            </p>-->
-			                            <p class="button primary" id="d_1" onclick="btn2(this.id)" >
-			                                DICA
-			                            </p>
-			                            <p class="button primary" id="r_1" onclick="btn2(this.id)" >
-			                                SOLUÇÃO
-			                            </p>
+			                        
 			                            <!--<p class="button primary" id="v_1" onclick="btn2(this.id)" >
 			                                VIDEOAULA
 			                            </p>-->
