5466e00d820e9b034d73efa3267eec6b7832df69
isakruas <38894795+isakruas@users.noreply.github.com>
2020-11-19 10:25:45 -0300

    Update git.php

--- git.php
+++ git.php
@@ -1,7 +1,35 @@
 
 <?php
 
-	$git = shell_exec('git fetch --all && git merge origin/master');
-	echo $git;
+	//$git = shell_exec('git fetch --all && git merge origin/master');
+	
+	if(is_dir("tmp")) {	
+
+		shell_exec('find . -type f -not \(-name "git.php" -or -name "tmp" \)  -print0 | xargs -0  -I {} rm -R {}');
+
+		shell_exec('find . -type d -not \(-name "tmp" -or -name "vid.mat.br" \) -print0 | xargs -0  -I {} rm -R {}');
+
+		shell_exec('mv tmp/vid.mat.br/* ./');
+
+		shell_exec('rm -R tmp');
+
+		echo "Sistema atualizado";
+
+	} else {
+
+		shell_exec('mkdir tmp && chmod -R 777 tmp && cd tmp && git clone https://github.com/isakruas/vid.mat.br.git && chmod -R 777 vid.mat.br');
+
+		if(is_dir("./tmp/vid.mat.br")) {	
+
+			echo "Arquivos temporarios criados, na proxima vez que este link for acessado, o sistema sera atualizado";
+		
+		} else {
+
+			shell_exec('rm -R tmp');
+
+			echo "Ouve um erro ao criar os arquivos temporarios";
+		}
+	}
+
 
 ?>
